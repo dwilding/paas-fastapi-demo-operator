7ad69b27872de466dc283a1b5e422d0e592ee4c6
Dave Wilding <tech@dpw.me>
2026-06-26 18:41:37 +0800

    add charm and ci

---
+++ tests/integration/test_charm.py
@@ -0,0 +1,29 @@
+# Copyright 2026 david.wilding@canonical.com
+# See LICENSE file for licensing details.
+#
+# The integration tests use the Jubilant library and the pytest-jubilant plugin.
+# See https://documentation.ubuntu.com/ops/latest/howto/write-integration-tests-for-a-charm/
+#
+# pytest-jubilant provides a module-scoped `juju` fixture that creates a temporary Juju model.
+# The `charm` fixture is defined in conftest.py.
+
+import logging
+import pathlib
+
+import jubilant
+import pytest
+import yaml
+
+logger = logging.getLogger(__name__)
+
+METADATA = yaml.safe_load(pathlib.Path("charmcraft.yaml").read_text())
+
+
+@pytest.mark.juju_setup
+def test_deploy(charm: pathlib.Path, juju: jubilant.Juju):
+    """Deploy the charm under test."""
+    resources = {
+        "app-image": METADATA["resources"]["app-image"]["upstream-source"]
+    }
+    juju.deploy(charm, app="generic", resources=resources)
+    juju.wait(jubilant.all_active)
